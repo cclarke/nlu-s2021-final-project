6f6e4fddcccc6d623be8950b5e1c66df85c18f2f
Cameron Clarke <cameron.clarke929@gmail.com>
2021-05-05 11:44:21 -0400

    add batched tokenization back in

--- experiment_script.py
+++ experiment_script.py
@@ -352,7 +352,8 @@ if __name__ == "__main__":
         dataset = dataset.map(
             lambda x: tokenizer(
                 x[TRAIN_FEATURES_COLUMN], truncation=True, padding=True
-            )
+            ),
+            batched=True
         )
 
         logging.info(f"Relabeling dataset column {TRAIN_LABELS_COLUMN} using {TRAINING_RELABEL_FUNC_NAME}")
