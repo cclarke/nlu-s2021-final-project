a61661abafba914e26b258822ba5684642b6d956
Cameron Clarke <cameron.clarke929@gmail.com>
2021-05-06 19:14:02 -0400

    move analysis relabel functions into a separate file

--- relabel_funcs.py
+++ relabel_funcs.py
@@ -1,13 +1,5 @@
-from torch.nn import Softmax
 import datasets
 from datasets import DatasetDict
-
-
-def _softmax_and_relabel(predictions, categorical_labels):
-    # takes in a torch.Tensor of predictions and a list of categorical labels, returns a tuple of (softmax tensor, categorical label)
-    m = Softmax(dim=0)
-    sm = m(predictions)
-    return sm, categorical_labels[sm.argmax().item()]
 
 
 def relabel_md_gender_yelp(dataset):
@@ -99,11 +91,6 @@ def split_relabel_jigsaw_identityhate(dataset):
     return dataset
 
 
-
-
-# -----------------------Social Bias Frames-------------------
-
-# Each dataset should have two functions, one that relabels the dataset, and another that turns the vector into a single score
 def relabel_sbic_offensiveness(dataset):
 
     def relabel_func(column):
@@ -163,22 +150,6 @@ def filter_relabel_sbic_targetcategory(dataset):
     return dataset
 
 
-def return_social_bias_frames(predictions):
-    # predictions for social bias frames is a 3 size vector with [not offensive, maybe offensive, offensive]
-    # returns a tuple of a score and a categorical variable
-    categorical_labels = ["Not", "Maybe", "Yes"]
-    softmax_preds, category = _softmax_and_relabel(predictions, categorical_labels)
-    # if maybe has the highest value, score is 0
-    # otherwise, take the difference between Yes and No
-    if category == "Maybe":
-        return 0, category
-    else:
-        return softmax_preds[2] - softmax_preds[0], category
-
-
-# -----------------------rtGender-------------------
-
-# Each dataset should have two functions, one that relabels the dataset, and another that turns the vector into a single score
 def split_relabel_rt_gender(dataset):
 
     def relabel_func(column):
@@ -200,17 +171,3 @@ def split_relabel_rt_gender(dataset):
     )
 
     return dataset
-
-
-
-def return_rt_gender(predictions):
-    # predictions for social bias frames is a 3 size vector with [not offensive, maybe offensive, offensive]
-    # returns a tuple of a score and a categorical variable
-    categorical_labels = ["Negative", "Neutral", "Mixed", "Positive"]
-    softmax_preds, category = _softmax_and_relabel(predictions, categorical_labels)
-    # if maybe has the highest value, score is 0
-    # otherwise, take the difference between Yes and No
-    if category == "Maybe":
-        return 0, category
-    else:
-        return softmax_preds[2] - softmax_preds[0], category
